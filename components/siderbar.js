bfe65c8265197c46aba3b701df96b4faa4d7e4cc
Ramy99-dev <63595300+Ramy99-dev@users.noreply.github.com>
2022-01-18 20:40:59 +0100

    add style to sidebar

--- components/siderbar.js
+++ components/siderbar.js
@@ -1,6 +1,17 @@
 import styles from '../styles/Sidebar.module.css'
+import { faHome ,faUser , faSignOutAlt} from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const SideBar = () => {
-    return ( <div className={styles.sidebar}>Sidebar is working</div> );
+    return ( <div className={styles.sidebar}>
+        <ul className={styles.listItems}>
+            <li><FontAwesomeIcon icon={faHome}/><span>Home</span></li>
+            <li><FontAwesomeIcon icon={faUser}/><span>Profile</span></li>
+            <li><FontAwesomeIcon icon={faSignOutAlt}/><span>Sign Out</span></li>
+        </ul>
+
+    </div> );
 }
  
 export default SideBar;
