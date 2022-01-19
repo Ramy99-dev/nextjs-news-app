f474cec7a317249596ed476c53d3262af20979fd
Ramy99-dev <63595300+Ramy99-dev@users.noreply.github.com>
2022-01-19 17:58:40 +0100

    add langagues

--- components/nav.js
+++ components/nav.js
@@ -1,27 +1,48 @@
 import styles from '../styles/Nav.module.css'
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {useEffect} from 'react';
+import {useEffect , useState} from 'react';
 
 const Nav = () => {
-    
+    const [countriesList , setCountries] = useState([]);
     useEffect(async ()=>{
+        
         const data = await  fetch("https://restcountries.com/v3.1/all");
         const countries = await data.json();
-        console.log(countries);
+        let countriesName = [];
+        countries.map((country)=>{
+            countriesName.push({name:country.name.common});
+        })
+        setCountries(countriesName)
     },[])
+
+
 
 
     return ( 
     <div className={styles.nav}>
         <div className={styles.options}>
-        <select className={styles.select}>
-            <option  value="San Francisco , California">San Francisco , California</option>
+      <select className={styles.select}>
+          {countriesList.map((country)=>{ 
+               return <option key={country.name} value={country?.name}>{country?.name}</option>
+          })}
+          
         </select>
         <select className={styles.select}>
             <option  value="Sports">Sports</option>
+            <option  value="Science">Science</option>
+            <option  value="Science">Culture</option>
+            <option  value="Science">Music</option>
+            <option  value="Science">Technology</option>
         </select>
-        <input className={styles.calendar} type="date" name="" id="" />
+
+        <select className={styles.select}>
+            <option  value="Sports">English</option>
+            <option  value="Science">Arabe</option>
+            <option  value="Science">Espagnol</option>
+            <option  value="Science">French</option>
+            <option  value="Science">Italien</option>
+        </select>
         </div>
         
        
